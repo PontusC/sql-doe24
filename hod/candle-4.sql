c78e79ed316a5c34d637ea77640637da800f4a21
Pontus Curtsson <pontuscurtsson@gmail.com>
2025-01-29 17:04:07 +0100

    solution hod-4

---
+++ hod/candle-4.sql
@@ -0,0 +1,17 @@
+-- Day 4
+-- Person who orders pastries (SKU starts with BKY, bakery?)
+-- orders early in the morning, before 5
+select name, phone, max(times_ordered) times_ordered
+from (
+    select name, phone, count(*) times_ordered
+from customers
+    join orders using (customerid)
+    join orders_items using (orderid)
+    join products using (sku)
+where
+    products.sku like 'BKY%' and
+    -- Below didnt work without casting as int, why?
+    cast (strftime('%H', orders.ordered) as int) between 0 and 5
+group by
+    name, phone
+);
