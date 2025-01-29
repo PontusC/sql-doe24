2369a043cbcc3d3f168e5817f2bae774debb3def
Pontus Curtsson <pontuscurtsson@gmail.com>
2025-01-29 17:07:02 +0100

    indentation fix

--- hod/candle-4.sql
+++ hod/candle-4.sql
@@ -4,14 +4,14 @@
 select name, phone, max(times_ordered) times_ordered
 from (
     select name, phone, count(*) times_ordered
-from customers
-    join orders using (customerid)
-    join orders_items using (orderid)
-    join products using (sku)
-where
-    products.sku like 'BKY%' and
-    -- Below didnt work without casting as int, why?
-    cast (strftime('%H', orders.ordered) as int) between 0 and 5
-group by
-    name, phone
+    from customers
+        join orders using (customerid)
+        join orders_items using (orderid)
+        join products using (sku)
+    where
+        products.sku like 'BKY%' and
+        -- Below didnt work without casting as int, why?
+        cast (strftime('%H', orders.ordered) as int) between 0 and 5
+    group by
+        name, phone
 );
