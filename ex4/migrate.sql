35723fb40e7e42f79fa648f69ac7dab710613c0a
Pontus Curtsson <pontuscurtsson@gmail.com>
2025-02-03 12:32:24 +0100

    setup

---
+++ ex4/migrate.sql
@@ -0,0 +1,12 @@
+-- Script to fix simpson.db
+-- either do step by step, or design the new tables first, then move data into them, then remove old
+
+-- Step 1. Rename tables, remove simpsons_ prefix
+
+-- Fix datatypes
+
+-- Fix naming, e.g. id to customer_id
+
+-- Fix primary keys
+
+-- Fix foreign keys and their datatypes
